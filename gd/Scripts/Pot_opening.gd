bb39ca61898b46714bd362b042277ba7a9262a24
r0ckwav3 <peterrockwave@gmail.com>
2024-03-01 14:40:08 -0800

    adjusted bubbles, added overboil

--- gd/Scripts/Pot_opening.gd
+++ gd/Scripts/Pot_opening.gd
@@ -7,30 +7,51 @@ extends Sprite2D
 # 1: overboiling
 # 2: green
 # 3: black
-
 @export var bubble_obj : PackedScene
-@export_category("Bubble Spawn Parameters") 
+@export_group("Bubble Spawn Parameters") 
 @export var bubble_spawn_offset : float
 @export var bubble_spawn_width : float
 @export var bubble_spawn_height : float
 @export var bubble_delay_min = 0.1
 @export var bubble_delay_max = 0.5 
+@export_group("State Parameters") 
+@export var overboil_multiplier = 0.1
 
 var bubble_cooldown = 0;
 
 func _process(delta):
 	bubble_cooldown -= delta;
 	if bubble_cooldown < 0:
-		bubble_cooldown = randf_range(bubble_delay_min, bubble_delay_max)
+		if state == 1:
+			bubble_cooldown = randf_range(bubble_delay_min, bubble_delay_max) * overboil_multiplier 
+		else:
+			bubble_cooldown = randf_range(bubble_delay_min, bubble_delay_max)
+			
 		_create_bubble()
 	
 func _create_bubble():
-	var bubble = bubble_obj.instantiate()
 	# select a point and then apply transformation from square to diamond
 	# 0,0 is bottom, 1,0 is left, 0,1 is right, 1,1 is top
+	if state != 1:
+		var bubble = bubble_obj.instantiate()
+		bubble.position = _calc_bubble_pos(randf(), randf())
+		add_child(bubble)
+	else:
+		# to make overboiling look good, spawn one bubble per quadrant
+
+		var bubble1 = bubble_obj.instantiate()
+		bubble1.position = _calc_bubble_pos(randf()*0.5, randf()*0.5)
+		add_child(bubble1)
+		var bubble2 = bubble_obj.instantiate()
+		bubble2.position = _calc_bubble_pos(0.5 + randf()*0.5, randf()*0.5)
+		add_child(bubble2)
+		var bubble3 = bubble_obj.instantiate()
+		bubble3.position = _calc_bubble_pos(randf()*0.5, 0.5 + randf()*0.5)
+		add_child(bubble3)
+		var bubble4 = bubble_obj.instantiate()
+		bubble4.position = _calc_bubble_pos(0.5 + randf()*0.5, 0.5 + randf()*0.5)
+		add_child(bubble4)
+
+func _calc_bubble_pos(dim1, dim2):
 	var base = Vector2(0, bubble_spawn_offset-(bubble_spawn_height/2))
-	var dim1 = randf() * Vector2(-bubble_spawn_width/2, bubble_spawn_height/2)
-	var dim2 = randf() * Vector2(bubble_spawn_width/2, bubble_spawn_height/2)
-	bubble.position = base + dim1 + dim2
-	add_child(bubble)
-	
+	return base + dim1 * Vector2(-bubble_spawn_width/2, bubble_spawn_height/2) + dim2 * Vector2(bubble_spawn_width/2, bubble_spawn_height/2)
